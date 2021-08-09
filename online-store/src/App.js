01648d657521d070a743c3fd37a84de45ecf0aba
Roman Kulkov <architect1of1pain@gmail.com>
2021-08-09 15:56:13 +0200

    Header

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -3,6 +3,7 @@ import{Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/Homepage/homepage.component';
 import ShopPage from './Pages/ShopPage/shoppage.component';
+import Header from './Components/Header/header.component';
 
 const HatsPage =()=>(
   <div>
@@ -13,6 +14,7 @@ const HatsPage =()=>(
 function App() {
   return (
     <div >
+      <Header/>
     <Switch>
       <Route exact path='/' component ={HomePage}/>
       <Route  path='/hats' component ={ShopPage}/>
