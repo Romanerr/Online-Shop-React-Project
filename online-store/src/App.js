edb94cad1c8ff94582ee71718b43c9a78096f973
Roman Kulkov <architect1of1pain@gmail.com>
2021-08-10 14:50:39 +0200

    SignIn

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './Pages/Homepage/homepage.component';
 import ShopPage from './Pages/ShopPage/shoppage.component';
 import Header from './Components/Header/header.component';
+import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.componemt.jsx';
 
 const HatsPage =()=>(
   <div>
@@ -18,6 +19,7 @@ function App() {
     <Switch>
       <Route exact path='/' component ={HomePage}/>
       <Route  path='/hats' component ={ShopPage}/>
+      <Route  path='/signin' component ={SignInAndSignUpPage}/>
     </Switch>
     </div>
   );
