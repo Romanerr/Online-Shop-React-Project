dcb165f57ac181a1b295cb1991711f3bdc79a955
Roman Kulkov <architect1of1pain@gmail.com>
2021-08-12 12:08:02 +0200

    Google auth button

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -5,17 +5,39 @@ import HomePage from './Pages/Homepage/homepage.component';
 import ShopPage from './Pages/ShopPage/shoppage.component';
 import Header from './Components/Header/header.component';
 import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.componemt.jsx';
-
+import {auth} from './Firebase/firebase.utils';
 const HatsPage =()=>(
   <div>
     <h1>Hats Page</h1>
   </div>
 )
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+render(){
   return (
     <div >
-      <Header/>
+      <Header currentUser={this.state.currentUser} />
     <Switch>
       <Route exact path='/' component ={HomePage}/>
       <Route  path='/hats' component ={ShopPage}/>
@@ -23,6 +45,6 @@ function App() {
     </Switch>
     </div>
   );
-}
+}}
 
 export default App;
